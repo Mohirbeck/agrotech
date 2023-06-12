8337f9d20161f38a288c30289493a065e751c969
Your Name <you@example.com>
2023-06-12 20:13:07 +0500

    base

--- config/settings.py
+++ config/settings.py
@@ -101,25 +101,25 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.{}'.format(
-#              os.getenv('DATABASE_ENGINE', 'sqlite3')
-#          ),
-#          'NAME': os.getenv('DATABASE_NAME', 'newsletter'),
-#          'USER': os.getenv('DATABASE_USERNAME', 'root'),
-#          'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-#          'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-#          'PORT': os.getenv('DATABASE_PORT', 5432),
-#      }
-#  }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DATABASE_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'newsletter'),
+         'USER': os.getenv('DATABASE_USERNAME', 'root'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
+ }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
